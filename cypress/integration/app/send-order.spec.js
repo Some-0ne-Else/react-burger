7ba061f0eb3419cd6993cafc374e128bb825bb31
Some-0ne-Else <cyberdef@gmail.com>
2021-09-14 19:13:57 +0300

    hotfix

--- cypress/integration/app/send-order.spec.js
+++ cypress/integration/app/send-order.spec.js
@@ -1,13 +1,5 @@
 describe('order sending after other tests', () => {
-//   before(() => {
-//     cy.visit('http://localhost:3000');
-//   });
-
-  //   it('should open constructor page by default', () => {
-  //     cy.contains('Соберите бургер');
-  //   });
-
-  it('should open feed page after button click', () => {
+  it('should open order popup after button click', () => {
     cy.get('button').contains('Оформить заказ').click();
     cy.contains('идентификатор заказа');
   });
